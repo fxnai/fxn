d8bb408002718aa736c0b87d2309e73ff689c8da
Yusuf Olokoba <olokobayusuf@gmail.com>
2023-05-30 08:54:26 -0400

    Add function magic

---
+++ fxn/magic.py
@@ -0,0 +1,30 @@
+# 
+#   Function
+#   Copyright © 2023 NatML Inc. All Rights Reserved.
+#
+
+from IPython.core.interactiveshell import InteractiveShell
+from IPython.core.magic import Magics, magics_class, line_magic
+from typing import List
+
+@magics_class
+class FunctionMagics (Magics):
+
+    @line_magic
+    def fxn (self, line: str):
+        COMMANDS = {
+            "python": self.__python
+        }
+        args = line.split(" ")
+        command = COMMANDS.get(args[0], None)
+        if command is not None:
+            command(args[1:])
+        else:
+            raise RuntimeError(f"Unrecognized Function command: {args[0]}")
+
+    def __python (self, args: List[str]):
+        version = args[0]
+        print(f"Predictor will use Python {version} when running on Function")
+
+def load_ipython_extension (ipython: InteractiveShell):
+    ipython.register_magics(FunctionMagics)
