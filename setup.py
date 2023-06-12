0b59ac921eabb6c2b34e3c11e317794a41b51a93
Yusuf Olokoba <olokobayusuf@gmail.com>
2023-06-12 09:37:12 -0400

    Minor mod

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     version=version,
     author="NatML Inc.",
     author_email="hi@fxn.ai",
-    description="Run on-device and cloud AI prediction functions in Python. Register at https://hub.fxn.ai.",
+    description="Run on-device and cloud AI prediction functions in Python. Register at https://fxn.ai.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="Apache License 2.0",
